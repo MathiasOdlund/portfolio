93adbcb2ace7fddcd92703bb372c8c4578625852
Thomas <azzpr@protonmail.com>
2022-07-14 14:58:59 +0200

    added google analytics

---
+++ client-ts/src/lib/analytics.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-NE8WYKTSD7', {
+				page_title: document.title,
+				page_path: $page.url.href
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-NE8WYKTSD7">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'MEASUREMENT_ID');
+	</script>
+</svelte:head>
--- client-ts/src/routes/__layout.svelte
+++ client-ts/src/routes/__layout.svelte
@@ -1,11 +1,12 @@
 <script>
+	import Analytics from '$lib/analytics.svelte';
 	import '../app.css';
 	// import Footer from "../components/footer/footer.svelte";
 	import Navbar from '../components/navbar/Navbar.svelte';
 </script>
 
 <svelte:head />
-
+<Analytics />
 <div class="">
 	<Navbar />
 
